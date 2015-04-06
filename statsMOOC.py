bb4b26b3e10a9f8cab0d1123ca3b4f5f8f937bdc
UberHowley <ihowley@gmail.com>
2015-04-06 01:02:34 -0400

    Adding in a little more descriptive stats
    Adding a tiny bit more descriptive statistics using pandas. Beginning to
    add in descriptive stats for each condition (although I'd like to break
    it down into percentage QH instances assigned to each condition).

--- statsMOOC.py
+++ statsMOOC.py
@@ -26,7 +26,15 @@ def run():
     print(data[utils.COL_NUMHELPERS].describe())
     print(utils.FORMAT_LINE)
 
-    # Descriptive Stats of conditions
+    # Descriptive Statistics of conditions
+    print(utils.FORMAT_LINE)
+    print("Descriptive statistics for: all conditions")
+    df_conditions = data[[utils.COL_BADGE, utils.COL_IRRELEVANT, utils.COL_VOTING, utils.COL_USERNAME, utils.COL_VERSION]]
+    print(df_conditions.describe())
+
+    # Descriptive Stats of individual conditions
+    # TODO: Determine how to get 'count' value for each condition in each dimension to provide % of instances in each
+    # TODO: Want to answer: "Percent instances assigned to badge/noBadge condition, vote/noVote, etc."
     print(utils.FORMAT_LINE)
     print("Descriptive statistics for: \'" + utils.COL_BADGE+"\'")
     print(data.groupby(utils.COL_BADGE)[utils.COL_BADGE].describe())
